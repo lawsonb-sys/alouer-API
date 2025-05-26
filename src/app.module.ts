ede178caaa4235c3c042cadba40d3c8709c51da8
late lawson <lateralezk@gmail.com>
2025-05-26 22:55:29 +0000

    ajout d'un db mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -39,7 +39,7 @@ import { UsersModule } from './users/users.module';
             connectTimeout: 60000,
             socketPath: null,
           },
-          synchronize: false,
+          synchronize: true,
           extra: {
             authPlugin: 'mysql_native_password',
           },
